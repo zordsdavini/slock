de00201e73097607d4b76e1a3ef8459613f9af64
Arnas Udovicius <arnas.u@estina.lt>
2019-11-26 14:11:25 +0200

    capslock patch

--- config.h
+++ config.h
@@ -3,9 +3,10 @@ static const char *user  = "arnas";
 static const char *group = "arnas";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+    [INIT] =   "black",     /* after initialization */
+    [INPUT] =  "blue",   /* during input */
+    [FAILED] = "red",   /* wrong password */
+    [CAPS] = "yellow",         /* CapsLock on */
 };
 
 /* treat a cleared input like a wrong password (color) */
