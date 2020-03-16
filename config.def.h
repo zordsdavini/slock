8f3815d030bc1b95bd7dd81f68ef7de8ce93eb5b
Arnas Udovicius <zordsdavini@gmail.com>
2020-03-16 17:31:39 +0200

    try to add fingerprint

--- config.def.h
+++ config.def.h
@@ -7,6 +7,7 @@ static const char *colorname[NUMCOLS] = {
 	[INPUT] = "#005577",    /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
 	[CAPS] = "red",         /* CapsLock on */
+    [PAM] = "#c9400D3",   /* waiting for PAM */
 };
 
 /*
@@ -17,7 +18,11 @@ ResourcePref resources[] = {
 		{ "color4",       STRING,  &colorname[INPUT] },
 		{ "color1",       STRING,  &colorname[FAILED] },
 		{ "color3",       STRING,  &colorname[CAPS] },
+		{ "color2",       STRING,  &colorname[PAM] },
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* PAM service that's used for authentication */
+static const char* pam_service = "login";
