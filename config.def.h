05fd3ee89f9d80b1e5ddb07a74da4849ea0f8e5a
Arnas Udovicius <zordsdavini@gmail.com>
2020-05-22 13:37:32 +0300

    Revert "try to add fingerprint"
    This reverts commit 8f3815d030bc1b95bd7dd81f68ef7de8ce93eb5b.

--- config.def.h
+++ config.def.h
@@ -7,7 +7,6 @@ static const char *colorname[NUMCOLS] = {
 	[INPUT] = "#005577",    /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
 	[CAPS] = "red",         /* CapsLock on */
-    [PAM] = "#c9400D3",   /* waiting for PAM */
 };
 
 /*
@@ -18,11 +17,7 @@ ResourcePref resources[] = {
 		{ "color4",       STRING,  &colorname[INPUT] },
 		{ "color1",       STRING,  &colorname[FAILED] },
 		{ "color3",       STRING,  &colorname[CAPS] },
-		{ "color2",       STRING,  &colorname[PAM] },
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
-
-/* PAM service that's used for authentication */
-static const char* pam_service = "login";
