70d89e5da7b05e9a9fa5d31fd51e370d984bb37e
Arnas Udovicius <zordsdavini@gmail.com>
2020-05-22 13:36:22 +0300

    Revert "changed group"
    This reverts commit 2b8634eb9ee2eea27a7fe02e151154b0edbc911a.

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 /* user and group to drop privileges to */
 static const char *user  = "arnas";
-static const char *group = "wheel";
+static const char *group = "arnas";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] = "black",       /* after initialization */
